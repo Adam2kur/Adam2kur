6dd6a6a8d8c342d95d0c5b546b8ddbc3b5c02355
Adam2kur <140524998+Adam2kur@users.noreply.github.com>
2023-09-23 04:19:55 -0700

    delete index2.js

--- jSource/index.js
+++ jSource/index.js
@@ -1,19 +1,11 @@
- class Test {
-    constructor(name, id)
-    {
-        this.name = name
-        this.id = id
-    }
-    print()
-    {
-        console.log(`my name is ${this.name} and my id is ${this.id}`)
-    }
- }
-     const obj = new Test('adamsy', 6819)
-     Test.prototype.init = function(name,id)
+  function name(data)
+  {
+    return new Promise((resolve,reject)=>{
+     if(true)
      {
-           this.name = name
-           this.id = id 
+        setTimeout(()=>resolve(data),2000)
      }
-     obj.init('pascal',1234)
-     obj.print()
+    })
+    .then(result => console.log(result))
+  }
+   name('adamsy')
