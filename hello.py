fa56922fb40fc7962cb4010c2f9be7f1b23c44a0
Adam2kur <140524998+Adam2kur@users.noreply.github.com>
2023-10-04 20:42:31 +0100

    added some new files

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-num = [1,2,3,4,6,6]
-num = num.__reduce__()
+
+class Number :
+    def __init__(self, data : str, id : int) :
+        if data == 'Adamsy' :
+            print(data +' welcome your id is ' + str(id))
+        else :
+            print('access not granted')
+            
+Number('Adamsy', 123)
+name = input(int(6))
+print(name) 
